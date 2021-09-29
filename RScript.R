9d88dc6722e418117e46880b0e0ef1197010f60c
Paige Appleagate <36541573+PaigeApplegate@users.noreply.github.com>
2021-09-29 16:17:25 -0400

    PlotPca graph

--- RScript.R
+++ RScript.R
@@ -1,11 +1,32 @@
-install.packages("ggplot2")
-
 data_file <- "GSE167171_AllCounts.txt/GSE167171_AllCounts.txt"
 
-d <- read.table(data_file, TRUE, sep="")
+data <- read.table(data_file, TRUE, sep="")
 
-#control + enter to assign objects
-print(d)
+countdata <- readRDS(file="./GSE167171_AllCounts.txt/countdata(2).Rda", refhook = NULL)
+countdata <- countdata[,-1]
+rownames(countdata) <- countdata[,1]
+
+dim(countdata)
+keep <- rowSums(countdata) > 5
+countdata <- countdata[keep,]
+dim(countdata)
+
+librarySizes <- colSums(countdata)
+barplot(librarySizes, 
+        names=names(librarySizes), 
+        las=2, 
+        main="Barplot of library sizes")
+
+logcounts <- log2(countdata + 1)
+boxplot(logcounts,
+        xlab="",
+        ylab="Log2(Counts)",
+        las=2)
+abline(h=median(as.matrix(logcounts)), col = "blue")
+
 
 library(ggplot2)
-ggplot(data=d, aes(x="MI2C5", y="Length"))
+library(ggfortify)
+rlogcounts <- rlog(countdata)
+pcDat <- prcomp(t(logcounts))
+autoplot(pcDat)
