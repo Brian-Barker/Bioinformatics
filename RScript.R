af718680e096c77891d52ef8883a47deb24b3ed2
LAD-reposit <lindsaydobrie@ufl.edu>
2021-10-19 18:05:03 -0400

    added changes from assignment2

--- RScript.R
+++ RScript.R
@@ -68,3 +68,72 @@ k <- enrichMKEGG(gene = "CICLE_v10004626mg",
                organism = 'cic',
                pvalueCutoff = 0.05)
 head(k)
+
+## this didn't upload, but 1 and 5 i think
+data_file <- "./data/GSE167171_AllCounts.txt"
+d <- read.table(data_file, TRUE, sep="")
+
+#####################
+# 1
+#####################
+#1c
+columns <- ncol(d) - 7
+rows <-nrow(d)
+cat("There are a total of ", toString(columns), " samples in the data.")
+cat("Additionally, there are a total of ", toString(rows), "genes that were differentially expressed in the sample")
+#1c - what is the variation in the data
+v <- d[, 7:ncol(d)]
+vary <- var(v)
+range(vary)
+#1c - per gene expression ranges
+rowranges <- numeric()
+for (i in 1:nrow(d)) {
+        rowranges <- c(rowranges, diff(range(d[i, 7:ncol(d)])))
+}
+ct <- fivenum(rowranges)
+upper <- ct[4] +(ct[4]-ct[2])*1.5
+lower <- ct[2] -(ct[4]-ct[2])*1.5
+rowranges <- rowranges[rowranges<=upper]
+dens <- density(rowranges)
+autoplot(dens, xlab="Range Values of Gene Expression", ylab="Density", main="Per-Gene Expression Ranges")
+
+#################
+# 4
+#################
+
+dds <- DESeqDataSetFromMatrix(countData = countdata, colData = groups, design = ~Group)
+dds <- DESeqDataSetFromMatrix(countData = countdata, colData = groups, design = ~Group + Month)
+dds <- DESeq(dds)
+results_dds <- results(dds)
+results_dds <- lfcShrink( dds, coef=2, res = results_dds)
+
+dds.df <- results_dds %>%
+        as.data.frame() %>%
+        tibble::rownames_to_column("Gene") %>%
+        dplyr::mutate(threshold = padj < 0.05) %>%
+        dplyr::arrange(dplyr::desc(log2FoldChange))
+
+select <- order(rowMeans(counts(dds,normalized=TRUE)),
+                decreasing=TRUE)
+
+#building heatmap
+col_fun = colorRamp2(c(-2, 0, 2), c("green", "white", "blue"))
+col_fun(seq(-3, 3))
+
+normz = results_dds$padj <= 0.01 & results_dds$baseMean >= 0.05 & 
+        abs(results_dds$log2FoldChange) >= 0; normz[is.na(normz)] = FALSE
+dds_m = counts(dds, normalized = TRUE)
+dds_m = dds_m[normz, ]
+
+heat = Heatmap(t(scale(t(dds_m))), name = "z-score",
+               top_annotation = HeatmapAnnotation(
+                       TreatmentGroups = colData(dds)$Group,
+                       sizeFactor = anno_points(colData(dds)$sizeFactor)
+               ),
+               show_row_names = FALSE, show_column_names = FALSE,
+               column_title = paste0(sum(normz), " Significant Genes Assuming FDR = 0.05"),
+               show_row_dend = FALSE)+
+        Heatmap(results_dds$log2FoldChange[normz], show_row_names = FALSE, width = unit(7, "mm"),
+                name = "log2FoldChange", show_column_names = FALSE, col=col_fun)
+heat = draw(heat, merge_legend = TRUE)
+heat
