2ca3406c0dd5b652d92b4a28383cc73802fe24f1
Brian-Barker <briantbarker@ufl.edu>
2021-10-20 20:33:08 -0400

    Update Assignment3-UnsupervisedDataAnalysis.R

--- Assignment3-UnsupervisedDataAnalysis.R
+++ Assignment3-UnsupervisedDataAnalysis.R
@@ -22,3 +22,16 @@ top5k <- apply(countdata, FUN=mad, MARGIN=1)
 top5k <- sort(top5k, decreasing=TRUE)
 top5k <- top5k[1:5000]
 top5k
+
+#1 Brian - Gaussian Mixture Models
+install.packages("mclust")
+library(mclust)
+top5kvar <- apply(countdata, FUN=var, MARGIN=1)
+top5kvar <- sort(top5kvar, decreasing=TRUE)
+top5kvar <- top5kvar[1:5000]
+top5kvar
+top5kvarframe = as.data.frame(top5kvar)
+mod <- mclust::Mclust(top5kvar)
+dr <- MclustDR(mod)
+
+clPairs(top5kvar, groups$Experimental)
